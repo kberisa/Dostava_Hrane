9914e565a10ee46bd0566dcbf7f60dd0ebcd41c3
Kristijan Berisa <k_berisa@hotmail.com>
2023-10-16 15:48:34 +0200

    update

--- DostavaHrane/DostavaHrane01/Controllers/DostavljacController.cs
+++ DostavaHrane/DostavaHrane01/Controllers/DostavljacController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DostavaHrane.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +15,52 @@ namespace DostavaHrane.Controllers
     [Route("api/v1/[controller]")]
     public class DostavljacController : ControllerBase
     {
+    }
+    [HttpGet]
+    public IActionResult Get()
+    {
+        _logger.LogInformation("Dohvaćam Dostavljace");
+
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        try
+        {
+            var dostavljaci = _context.Dostavljac
+                .Include(d => d.Dostavljac)
+                .Include(k => k.Kosarica)
+                .ToList();
+
+            if (dostavljaci == null || dostavljaci.Count == 0)
+            {
+                return new EmptyResult();
+            }
+
+            List<DostavljacDTO> vrati = new();
+
+            dostavljaci.ForEach(d =>
+            {
+                vrati.Add(new DostavljacDTO()
+                {
+                    Sifra = d.Sifra,
+                    Ime = d.Ime,
+                    Smjer = g.Smjer?.Naziv,
+                    SifraSmjer = g.Smjer.Sifra,
+                    DatumPocetka = g.DatumPocetka,
+                    BrojPolaznika = g.Polaznici.Count
+                });
+            });
+            return Ok(vrati);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(
+                StatusCodes.Status503ServiceUnavailable,
+                ex);
+        }
+
+
     }
 }
--- DostavaHrane/DostavaHrane01/Controllers/KupacController.cs
+++ DostavaHrane/DostavaHrane01/Controllers/KupacController.cs
@@ -1,6 +1,7 @@
 ﻿using DostavaHrane.Data;
 using DostavaHrane.Models;
 using DostavaHrane.Models.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.InteropServices;
@@ -12,4 +13,77 @@ namespace DostavaHrane.Controllers
     public class KupacController : ControllerBase
     {
     }
+    [HttpGet]
+    public IActionResult Get()
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var kupci = _context.Polaznik.ToList();
+        if  kupci == null || kupci.Count == 0)
+        {
+            return new EmptyResult();
+        }
+
+        List<KupacDTO> vrati = new();
+
+        kupci.ForEach(k =>
+        {
+            // ovo je ručno presipavanje, kasnije upogonimo automapper
+            var kdto = new KupacDTO()
+            {
+                Sifra = k.Sifra,
+                KosrisnickoIme = k.KosrisnickoIme,
+                Ime = k.Ime,
+                Prezime = k.Prezime,
+                Telefon = k.Telefon,
+                Adresa = k.Adresa
+            };
+
+            vrati.Add(kdto);
+
+
+        });
+
+
+        return Ok(vrati);
+
+    }
+
+
+
+    [HttpGet]
+    [Route("{sifra:int}")]
+    public IActionResult GetBySifra(int sifra)
+    {
+
+        if (sifra <= 0)
+        {
+            return BadRequest(ModelState);
+        }
+
+        try
+        {
+            var s = _context.Kupac.Find(sifra);
+
+            if (s == null)
+            {
+                return StatusCode(StatusCodes.Status204NoContent, s);
+            }
+
+            return new JsonResult(s);
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status503ServiceUnavailable, ex.Message);
+        }
+
+    }
+
+
+
+
 }
